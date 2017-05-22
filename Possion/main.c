1aa7751995ebc362e1d06252f8e26f617c637cfb
imweixin <fulmouse@163.com>
2017-05-22 13:37:04 +0800

    OK!BYE BYE

--- Possion/main.c
+++ Possion/main.c
@@ -1,146 +1,98 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
-#include<string.h>
 
-bool check(int a, int b, int c);
-int * get_bottles();
-void move(int *start,int count);
-void addPath(int *from);
-bool contain(int *get);
+void addPath(int *start, int number,int length);
+int* move(int *start,int length);
+bool contain(int *start);
 
+
+int ** path;
+int number = 0;
 int *max, *start, *end;
-int *path;
-int length=0; //定义路径长度
-
-FILE * fp;
 
 int main() {
-	do {
+	/*do {
 	printf("请输入各瓶子最多装酒数量,用空格隔开(例如:1 2 3):\n");
 	max = get_bottles();
 	printf("请输入各瓶子初始装酒数量,用空格隔开(例如:1 2 3):\n");
-	start = get_bottles();
+	start= get_bottles();
 	printf("请输入各瓶子要求装酒数量,用空格隔开(例如:1 2 3):\n");
 	end = get_bottles();
-	} while (check(*max, *start, *end));
-
-	fp = fopen("path.txt", "w+");
-	if (!fp) {
-		printf("打开文件失败");
-	}
-	fclose(fp);
-	/*int max[4] = { 3,12,8,5 };
-	int start[4] = { 3,12,0,0 };
-	int end[4] = { 3,6,6,0 };*/
-
+	} while (check(maxVolume[0], iniVolume[0], demandVolume[0]));*/
+	int maxm[4] = { 3,12,8,5 };
+	int ini[4] = { 3,12,0,0 };
+	int dem[4] = { 3,6,6,0 };
+	max = maxm, start = ini, end = dem;
 	move(start,0);
-	free(max);
-	free(start);
-	free(end);
+	
 	system("pause");
 	return 0;
 }
 
-void move(int *start ,int count) {
-	bool isMoved=false;
-	path = (int *)realloc(path,(*start + 1)*(length+1) *sizeof(int));    //*start+3
-	
+void goPath() {
 
-	addPath(start);
+	for (int i = 0; i < number; i++) {
+		
+		while (true) {
+			move(start,0);
+		}
+	}
 
+}
+
+int* move(int *start,int length) {
+	int number = 0;
 	for (int i = 1; i <= *start; i++) {
 		for (int j = 1; j <= *start; j++) {
-			int a = start[i], b = start[j];      //临时变量,用于存储start[i]和start[j]的值
-			if (i == j) {
-				isMoved = false;
+
+			int a = start[i], b = start[j];
+			if (i == j || start[i] == 0 || start[j] == max[j]) {
 				continue;
 			}
-			if (start[i] == 0 || start[j] == max[j]) {
-				isMoved = false;
-				continue;
-			}
-			else if(start[i]>=max[j]-start[j]){
+			else if (start[i] > max[j] - start[j]) {
 				start[i] -= max[j] - start[j];
 				start[j] = max[j];
-				if (contain(start)) {
-					start[i] = a, start[j] = b;
-					isMoved = false;
-					continue;
-				}
-				else {
-					isMoved = true;
-				}
 			}
 			else {
 				start[j] += start[i];
 				start[i] = 0;
-				if (contain(start)) {
-					start[i] = a, start[j] = b;
-					isMoved = false;
-					continue;
-				}
-				else {
-					isMoved = true;
-				}
 			}
-			if (isMoved) {
+
+			if (start[i] != a && !contain(start)) {
 				printf("%d->%d: ", i, j);
 				for (int k = 1; k <= *start; k++) {
 					printf("%d ", start[k]);
 				}
-				printf("\n");
+				addPath(start,number,length);
+				number++;
 			}
-
-			fp = fopen("path.txt", "a+");
-			fprintf(fp ,"%d->%d: ", i, j);
-			for (int i = 1; i <= *start; i++) {
-				fprintf(fp, "%d ", start[i]);
-			}
-			fprintf(fp, "\n");
-			fclose(fp);
+			printf("\n");
+			start[i] = a;
+			start[j] = b;
 
 		}
 	}
-	
-	printf("\n\n\n");
-	/*free(path);
-	path = (int*)malloc(sizeof(int));
-	move(start, 0);
-	count++;
-
-	printf("%d\n",length);*/
-
+	return start;
 }
 
-void addPath(int *from) {
-	for (int i = 0; i <= *from; i++) {
-		path[(*start + 1)*length + i] = from[i];
-	}
+void addPath(int *start,int number,int length) {
 	length++;
+	path = (int **)realloc(path,number * sizeof(int *));
+	for (int i = 0; i < length; i++) {
+		path[i] = (int *)realloc(path[i], (*start + 1)*length * sizeof(int));
+	}
+
+	//path[number] = (int *)realloc(path[number],(*start+1)*length*sizeof(int));
+	for (int i = 0; i <= *start; i++) {
+		path[number][(*start + 1)*length + i] = start[i];
+	}
+
 }
 
-bool contain(int *get) {
-	bool mark;
-	for (int i = 0; i < length; i++) {
-		mark = true;
-		for (int j = 1; j <= *get; j++) {
-			if (path[i*(*get + 1) + j] != get[j]) {
-				mark = false;
-				break;
-			}
-		}
-		if (mark) {
-			return true;
-		}
-	}
+bool contain(int *start) {
 	return false;
 }
-
-
-
-
 
 
 
