62d2c5873bf1bc8c63c7107760eee1f36fc6375d
imweixin <fulmouse@163.com>
2017-05-22 23:36:05 +0800

    first complete!

--- Possion/func.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef _BOTTLES_
-extern int* get_bottles();
-extern bool check(int a, int b, int c); //检测三次输入瓶子数量是否相等
-extern void writePath(int from, int to, int *start);
-
-extern int num;
-#endif // !_BOTTLES_
--- Possion/main.c
+++ Possion/main.c
@@ -1,13 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
+#include<string.h>
 
-void addPath(int *start, int number,int length);
-int* move(int *start,int length);
-bool contain(int *start);
+void goPath(int *start);
+int addPath(int *start, int temp, int number, int length);
+int move(int *start, int length,int nownum);
+bool contain(int *start, int number, int length);
+bool isequal(int *start, int  *end);
+void printPath();
 
 
-int ** path;
+int path[1024][1024];
 int number = 0;
 int *max, *start, *end;
 
@@ -24,30 +29,56 @@ int main() {
 	int ini[4] = { 3,12,0,0 };
 	int dem[4] = { 3,6,6,0 };
 	max = maxm, start = ini, end = dem;
-	move(start,0);
-	
+	goPath(start);
+	printPath();
 	system("pause");
 	return 0;
 }
 
-void goPath() {
+void goPath(int *start) {
+	addPath(start, 0, 0, 0);
 
-	for (int i = 0; i < number; i++) {
-		
-		while (true) {
-			move(start,0);
+
+	/*move(start, 1, 0);
+	int ini[4] = { 3,4,8,0 };
+	start = ini;
+	move(start, 2, 0);
+	int ini1[4] = {3,7,0,5 };
+	start = ini1;
+	move(start, 2, 1);*/
+
+	//int branch=move(start, 1,0);
+
+
+	for (int i = 0; i <= number; i++) {
+		if (i == number) {
+			i = 0;
+		}
+		int length = path[number][0];
+		int ini[10];
+		start = &path[number][2 + (length - 1)*(*start + 1)];
+		memcpy(ini, start, (*start + 1) * sizeof(int));		
+		move(ini, length, path[number][1]);
+		if (number == 13) {
+			break;
 		}
 	}
 
+	printf("%d\n", number);
+
 }
 
-int* move(int *start,int length) {
-	int number = 0;
+int move(int *start, int length,int nownum) {
+	int branch = 0;
+
 	for (int i = 1; i <= *start; i++) {
 		for (int j = 1; j <= *start; j++) {
 
 			int a = start[i], b = start[j];
 			if (i == j || start[i] == 0 || start[j] == max[j]) {
+				if (isequal(start, end)) {
+					printf("\n移动完成\n");
+				}
 				continue;
 			}
 			else if (start[i] > max[j] - start[j]) {
@@ -59,41 +90,91 @@ int* move(int *start,int length) {
 				start[i] = 0;
 			}
 
-			if (start[i] != a && !contain(start)) {
-				printf("%d->%d: ", i, j);
+			if (start[i] != a && !contain(start, nownum, length)) {
+				/*printf("%d->%d: ", i, j);
 				for (int k = 1; k <= *start; k++) {
 					printf("%d ", start[k]);
+				}*/
+				if (!branch) {
+					addPath(start, nownum, nownum, length);
 				}
-				addPath(start,number,length);
-				number++;
+				else {
+					number++;
+					addPath(start, nownum, number, length);
+				}
+				branch++;
 			}
-			printf("\n");
+			//printf("\n");
 			start[i] = a;
 			start[j] = b;
 
 		}
 	}
-	return start;
+	return branch;
 }
 
-void addPath(int *start,int number,int length) {
-	length++;
-	path = (int **)realloc(path,number * sizeof(int *));
-	for (int i = 0; i < length; i++) {
-		path[i] = (int *)realloc(path[i], (*start + 1)*length * sizeof(int));
+int addPath(int *start, int nownum, int number, int length) {
+	path[nownum][0] = length+1;
+	if (length&&number != nownum) {
+		for (int i = 0; i < (*start + 1)*length + 2; i++) {
+			path[number][i] = path[nownum][i];
+		}
 	}
 
-	//path[number] = (int *)realloc(path[number],(*start+1)*length*sizeof(int));
 	for (int i = 0; i <= *start; i++) {
-		path[number][(*start + 1)*length + i] = start[i];
+		path[number][1] = number;  //可以放for前面
+		path[number][(*start + 1)*length + i + 2] = start[i];
 	}
-
+	length++;
+	return length;
 }
 
-bool contain(int *start) {
+bool contain(int *start, int number, int length) {
+	bool mark;
+	for (int i = 0; i < length; i++) {
+		for (int j = 1; j <= *start; j++) {
+			if (path[number][(*start + 1)*i + j+2] == start[j]) {
+				mark = true;
+			}
+			else {
+				mark = false;
+				break;
+			}
+		}
+		if (mark) {
+			return true;
+		}
+	}
 	return false;
 }
 
+bool isequal(int *start, int  *end) {
+	for (int i = 1; i <= *end; i++) {
+		if (start[i] != end[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void printPath() {
+	int numel = path[number][2]; //记录瓶子数
+	FILE *fp = fopen("path.txt", "w");
+	for (int i = 0; i <= number; i++) {
+		for (int j = 0; j < path[i][0]; j++) {
+			for (int k = 0; k < numel; k++) {
+				printf("%4d ", path[i][(numel + 1)*j + 3+k]);
+				fprintf(fp, "%4d ", path[i][(numel + 1)*j + 3 + k]);
+			}
+			printf("\n");
+			fprintf(fp, "\n");
+		}
+		printf("\n\n");
+		fprintf(fp, "\n\n");
+	}
+	fclose(fp);
+}
 
 
 
