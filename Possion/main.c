25145bfb313cf7e3d654d1adb4aaa4960d84d534
imweixin <fulmouse@163.com>
2017-05-23 12:19:45 +0800

    need some optimization!

--- Possion/main.c
+++ Possion/main.c
@@ -6,15 +6,17 @@
 
 void goPath(int *start);
 int addPath(int *start, int temp, int number, int length);
-int move(int *start, int length,int nownum);
+int move(int *start, int length, int nownum);
 bool contain(int *start, int number, int length);
 bool isequal(int *start, int  *end);
 void printPath();
 bool noRepeat();
 
 
-int path[1024][1024];
-int number = 0;  //path的总数
+int path[1024][512];
+bool markEnd[1024] = { false };  //记录达成要求的路径
+bool endPath[1024] = { false };  //记录一条路径的结束(达到要求或者不管怎么移动都会重复前面的状态)
+int number = 0;                  //path的总数
 int *max, *start, *end;
 
 int main() {
@@ -44,84 +46,87 @@ int main() {
 
 void goPath(int *start) {
 	addPath(start, 0, 0, 0);
+	bool mark = false;
 
+	while (!mark) {
+		int temp = number;
 
-	/*move(start, 1, 0);
-	int ini[4] = { 3,4,8,0 };
-	start = ini;
-	move(start, 2, 0);
-	int ini1[4] = {3,7,0,5 };
-	start = ini1;
-	move(start, 2, 1);*/
-
-	//int branch=move(start, 1,0);
-
-
-	for (int i = 0; i <= number; i++) {
-		if (i == number) {
-			i = 0;
-		}
-		int length = path[i][0];
-		int ini[10];
-		start = &path[i][2 + (length - 1)*(*start + 1)];
-		memcpy(ini, start, (*start + 1) * sizeof(int));		
-		move(ini, length, path[i][1]);
-		if (number == 455) {
-			break;
-		}
-	}
-
-	printf("%d\n", number);
-
-}
-
-int move(int *start, int length,int nownum) { //length为当前path的长度,最短为1,nownum为当前path的序号,最小为0
-	int branch = 0;
-
-	for (int i = 1; i <= *start; i++) {
-		for (int j = 1; j <= *start; j++) {
-
-			int a = start[i], b = start[j];
-			if (i == j || start[i] == 0 || start[j] == max[j]) {
-				/*if (isequal(start, end)) {
-					printf("\n移动完成\n");
-				}*/
+		for (int i = 0; i <= temp; i++) {
+						
+			int length = path[i][0];
+			int ini[10];
+			start = &path[i][2 + (length - 1)*(*start + 1)];
+			memcpy(ini, start, (*start + 1) * sizeof(int));
+			int branch = 0;
+			int nownum = path[i][1];
+			if (endPath[nownum]) {
 				continue;
 			}
-			else if (start[i] > max[j] - start[j]) {
-				start[i] -= max[j] - start[j];
-				start[j] = max[j];
+			else {
+				branch = move(ini, length, nownum);
+			}
+			if (!branch) {
+				endPath[nownum] = true;
+			}
+
+		}
+		for (int i = 0; i <= temp; i++) {
+			if (endPath[path[i][1]]) {
+				mark = true;
 			}
 			else {
-				start[j] += start[i];
-				start[i] = 0;
+				mark = false;
+				break;
 			}
+		}
 
-			if (start[i] != a && !contain(start, nownum, length)) {
-				/*printf("%d->%d: ", i, j);
-				for (int k = 1; k <= *start; k++) {
-					printf("%d ", start[k]);
-				}*/
-				if (!branch) {
-					addPath(start, nownum, nownum, length);
+	}
+}
+
+int move(int *start, int length, int nownum) { //length为当前path的长度,最短为1,nownum为当前path的序号,最小为0
+	int branch = 0;
+
+	if (isequal(start, end)) {
+		markEnd[nownum] = true;
+	}
+	else {
+		for (int i = 1; i <= *start; i++) {
+			for (int j = 1; j <= *start; j++) {
+
+				int a = start[i], b = start[j];
+				if (i == j || start[i] == 0 || start[j] == max[j]) {
+					continue;
+				}
+				else if (start[i] > max[j] - start[j]) {
+					start[i] -= max[j] - start[j];
+					start[j] = max[j];
 				}
 				else {
-					number++;
-					addPath(start, nownum, number, length);
+					start[j] += start[i];
+					start[i] = 0;
 				}
-				branch++;
-			}
-			//printf("\n");
-			start[i] = a;
-			start[j] = b;
 
+				if (start[i] != a && !contain(start, nownum, length)) {
+					if (!branch) {
+						addPath(start, nownum, nownum, length);
+					}
+					else {
+						number++;
+						addPath(start, nownum, number, length);
+					}
+					branch++;
+				}
+				start[i] = a;
+				start[j] = b;
+
+			}
 		}
 	}
 	return branch;
 }
 
 int addPath(int *start, int nownum, int number, int length) {
-	path[nownum][0] = length+1;
+	path[nownum][0] = length + 1;
 	if (length&&number != nownum) {
 		for (int i = 0; i < (*start + 1)*length + 2; i++) {
 			path[number][i] = path[nownum][i];
@@ -140,7 +145,7 @@ bool contain(int *start, int number, int length) {
 	bool mark;
 	for (int i = 0; i < length; i++) {
 		for (int j = 1; j <= *start; j++) {
-			if (path[number][(*start + 1)*i + j+2] == start[j]) {
+			if (path[number][(*start + 1)*i + j + 2] == start[j]) {
 				mark = true;
 			}
 			else {
@@ -166,25 +171,31 @@ bool isequal(int *start, int  *end) {
 }
 
 void printPath() {
-	int numel = path[number][2];                //记录瓶子数
+	int endPath = 0;
+	int numel = path[number][2];                    //记录瓶子数
 	FILE *fp = fopen("path.txt", "w");
-	for (int i = 0; i <= number; i++) {         //路径数
-		for (int j = 0; j < path[i][0]; j++) {  //路径长度
-			for (int k = 0; k < numel; k++) {
-				printf("%4d ", path[i][(numel + 1)*j + 3+k]);
-				fprintf(fp, "%4d ", path[i][(numel + 1)*j + 3 + k]);
+	for (int i = 0; i <= number; i++) {             //路径数
+		if (markEnd[i]) {
+			endPath++;
+			printf("第%d条完成路径,序号为%d\n步数为%d\n", endPath, path[i][1],path[i][0] - 1);
+			fprintf(fp, "第%d条完成路径,序号为%d\n步数为%d\n", endPath, path[i][1], path[i][0] - 1);
+			for (int j = 0; j < path[i][0]; j++) {  //路径长度
+				for (int k = 0; k < numel; k++) {
+					printf("%4d ", path[i][(numel + 1)*j + 3 + k]);
+					fprintf(fp, "%4d ", path[i][(numel + 1)*j + 3 + k]);
+				}
+				printf("\n");
+				fprintf(fp, "\n");
 			}
-			printf("\n");
-			fprintf(fp, "\n");
+			printf("\n\n");
+			fprintf(fp, "\n\n");
 		}
-		printf("\n\n");
-		fprintf(fp, "\n\n");
 	}
 	fclose(fp);
 }
 
 bool noRepeat() {
-	int numel = path[0][2]+1;
+	int numel = path[0][2] + 1;
 	bool mark = true;
 	for (int i = 0; i <= number; i++) {
 		for (int j = 0; j <= number; j++) {
